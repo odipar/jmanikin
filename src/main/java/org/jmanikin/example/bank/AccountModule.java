801150a680f1f107be87a883831cbed12c869ad5
Robbert van Dalen <robbert@availlang.org>
2021-03-02 15:22:25 +0100

    oops

--- src/main/java/org/jmanikin/example/bank/AccountModule.java
+++ src/main/java/org/jmanikin/example/bank/AccountModule.java
@@ -21,12 +21,11 @@ public interface AccountModule {
         public final Double initial;
         public Open(Double initial) { this.initial = initial; }
     
-        @Override public Msg<W, ID, Account, Void> local() {
-            return
-                pre(() -> true).
-                app(() -> obj()).
-                eff(null).
-                pst(() -> true);
+        @Override public Msg<W, ID, Account, Void> local() { return
+            pre(() -> true).
+            app(() -> new Account(initial)).
+            eff(() -> null).
+            pst(() -> obj().balance == initial);
         }
     }
     
